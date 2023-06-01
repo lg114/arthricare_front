7f1c833b6d49e410a9cfcd67bd69a372d6b4fafa
Lin Gan <charlesgan0801@gmail.com>
2023-06-01 18:33:57 +1000

    read me

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import App from './App.vue';
 import 'element-plus/dist/index.css';
-import * as Icons from '@element-plus/icons'
 import router from './router';
 
 //Use Element Plus, Router
 createApp(App).use(ElementPlus).use(router).mount('#app');
 //Use icons from Element Plus
-Object.keys(Icons).forEach(key => {
-    App.component(key, Icons[key])
-  })
