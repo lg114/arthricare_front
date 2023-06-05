590b986f96ab0a54a24d8020433e81965f13e208
TomBranderererer <90673775+TomBranderererer@users.noreply.github.com>
2023-06-05 17:03:53 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import WelcomePage from '@/views/WelcomeView.vue';
+import LoginPage from '@/views/LoginView.vue';
+import SignUpPage from '@/views/SignupView.vue';
+import ResetPwdPage from '@/views/ResetpwdView.vue';
+import AddMedPage from '@/views/addMed.vue';
+
+const router  = createRouter({
+    history : createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'Welcome',
+            component: WelcomePage,
+        },
+        {
+            path: '/Login',
+            name: 'Login',
+            component: LoginPage,
+        },
+        {
+            path: '/Resetpassword',
+            name: 'Resetpassword',
+            component: ResetPwdPage,
+        },
+        {
+            path: '/Signup',
+            name: 'Signup',
+            component: SignUpPage,
+        },
+        {
+          path: '/AddMed',
+          name: 'AddMed',
+          component: AddMedPage,
+      }
+    ]
+})
+
+export default router;
