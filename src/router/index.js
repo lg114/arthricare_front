dc96600658eb9be437690389a3b3f0274b66de88
Sarakurachi91 <67615304+Sarakurachi91@users.noreply.github.com>
2023-08-11 16:30:16 +1000

    111

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+<<<<<<< Updated upstream
+=======
+// import { ElMessage } from 'element-plus';
+
+>>>>>>> Stashed changes
 import WelcomePage from '@/views/WelcomeView.vue';
 import LoginPage from '@/views/LoginView.vue';
 import SignUpPage from '@/views/SignupView.vue';
@@ -9,6 +14,14 @@ import MenuPage from '@/views/MenuView.vue';
 import UserProfilePage from '@/views/UserProfile.vue';
 import EditMedPage from '@/views/editMed.vue';
 import MyMedsPage from '@/views/MyMedsView.vue';
+<<<<<<< Updated upstream
+=======
+import RewardsPage from '@/views/RewardsView.vue';
+import CommunityPage from '@/views/CommunityView.vue';
+
+
+
+>>>>>>> Stashed changes
 
 const router  = createRouter({
     history : createWebHashHistory(),
@@ -62,8 +75,53 @@ const router  = createRouter({
             path: '/MyMeds',
             name: 'MyMeds',
             component: MyMedsPage,
+<<<<<<< Updated upstream
         }
     ]
 })
+=======
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/Rewards',
+            name: 'Rewards',
+            component: RewardsPage,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/Community',
+            name: 'Community',
+            component: CommunityPage,
+            meta: {
+                requiresAuth: true
+            }
+        }
+    ]
+});
+
+//Router Guard 导航守卫
+
+// router.beforeEach((to, from, next) => {
+//     const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
+//     if (loggedInUser) {
+//         if (to.meta.requiresAuth) {
+//             next(); // 已登录且可以访问需要登录的页面
+//         } else {
+//             next(); // 已登录，但可以访问不需要登录的页面
+//         }
+//     } else {
+//       if (to.meta.requiresAuth) {
+//         ElMessage.warning("You haven't log in.");
+//         next({ name: 'Welcome' }); //重定向 redirected
+//       } else {
+//         next(); // 未登录，可以访问不需要登录的页面
+//       }
+//     }
+//   });
+>>>>>>> Stashed changes
 
 export default router;
---
+++ src/views/CommunityView.vue
@@ -0,0 +1,129 @@
+<script>
+    import { ref } from 'vue';
+    import { AccountCircleRound, ModeEditOutlineOutlined, MoreHorizFilled, MedicationOutlined, CardGiftcardOutlined, HomeRound, AccountCircleOutlined, AddCircleFilled, AddCircleOutlineFilled, ChevronLeftRound } from '@vicons/material';
+    import { Icon } from '@vicons/utils'
+
+    import SideBarContent from '@/component/Sidebar.vue';
+    import { UserFilled } from '@element-plus/icons-vue';
+    export default{
+        mounted() {
+                document.title = "Community | ArthriCare";
+        },
+        data(){
+            return{
+                user:{
+                    name: 'Username',
+                    level: '10',
+                    points: '1000'
+                },
+                drawer: ref(false),
+            };
+        },
+        methods:{
+            openDrawer() {
+            this.drawer = true;
+            },
+            beforeDrawerClose(done) {
+                done();
+            },
+            //Router
+            goToUserProfile(){
+                this.$router.push('/Community');
+            }
+        },
+        components: {
+            Icon,
+            MoreHorizFilled,
+            MedicationOutlined, 
+            CardGiftcardOutlined, 
+            HomeRound, 
+            AccountCircleOutlined,
+            AddCircleFilled,
+            AddCircleOutlineFilled,
+            AccountCircleRound,
+            ModeEditOutlineOutlined,
+            SideBarContent,
+            UserFilled,
+            ChevronLeftRound
+        }
+    };
+</script>
+
+<template>
+    <div class="container">
+        <el-container>
+            <el-header class="header">
+                <Icon class="more" @click="drawer = true"><MoreHorizFilled /></Icon>
+                <Icon class="user"><AccountCircleRound /></Icon>
+                <b class="pageTitle">My Profile</b>
+                <Icon class="edit"><ModeEditOutlineOutlined /></Icon>
+            </el-header>
+            <el-main class="main">
+                <el-avatar class="avatar">
+                    <img src="@/assets/user_avatar.png"/>
+                </el-avatar> 
+                <h2>{{ user.name }}</h2>
+                <p>Level {{ user.level }} | {{ user.points }} points</p>
+                <div class="box">
+                    <text class="boxTitle">My Puzzles</text><br><br>
+                    <img src="@/assets/pic_1.jpg" class="collectionImage" :fit="cover"/>
+                    <img src="@/assets/pic_2.jpg" class="collectionImage" :fit="cover"/>
+                    <img src="@/assets/pic_3.png" class="collectionImage" :fit="cover"/><br>
+                    <Icon class="add"><AddCircleOutlineFilled /></Icon>
+                </div>
+                <div class="box">
+                    <text class="boxTitle">My Friends</text><br><br>
+                    <img src="@/assets/friend_1.png" class="friend_avatar"/>
+                    <img src="@/assets/friend_2.png" class="friend_avatar"/>
+                    <img src="@/assets/friend_3.png" class="friend_avatar"/>
+                    <img src="@/assets/friend_4.png" class="friend_avatar"/>
+                    <img src="@/assets/friend_5.png" class="friend_avatar"/>
+                    <Icon class="add"><AddCircleOutlineFilled /></Icon>
+                </div>
+                <div class="box">
+                    <text class="boxTitle">My Posts</text><br><br>
+                    <text class="postText">You don't have any post yet...</text>
+                    <Icon class="add"><AddCircleOutlineFilled /></Icon>
+                </div>
+                <Icon><ChevronLeftRound/></Icon>
+            </el-main> 
+        </el-container>
+        <el-footer class="footer">
+                <router-link to = "/Home">
+                        <Icon class="footerBtn" id="home"><HomeRound /></Icon>      
+                </router-link>
+                <router-link to = "/MyMeds">
+                    <Icon class="footerBtn" id="medication"><MedicationOutlined /></Icon>  
+                </router-link>
+                <router-link to = "/AddMed">
+                    <Icon class="footerBtn" id="addMed"><AddCircleFilled /></Icon>  
+                </router-link>
+                <router-link to = "/Rewards">
+                <Icon class="footerBtn" id="rewards"><CardGiftcardOutlined /></Icon>
+                </router-link>
+                <Icon class="footerBtn" id="profile"><AccountCircleOutlined /></Icon><br>
+                <span id="homeText">Home</span>
+                <span id="medText">My Meds</span>
+                <span id="rewardsText">Rewards</span>
+                <span id="profileText">Profile</span>
+        </el-footer>
+        <el-drawer style="background-color: #1890FF;" v-model="drawer" title="sidebar" :with-header="false" direction="ltr" size="70%" :append-to-body = "true" :before-close = "beforeDrawerClose">
+            <!--Action是模拟接口，与后端连接时更换-->
+                <div class = "sidebar">
+                    <el-upload action="" :show-file-list="false">
+                        <el-avatar :size="65">
+                            <img :src="imgUrl" v-if="imgUrl" class="uploaded-avatar" />
+                                <template v-else>
+                                    <UserFilled class="defalut-avatar" />
+                                </template>
+                        </el-avatar>   
+                    </el-upload> 
+                </div>
+            <SideBarContent :imgUrl="imgUrl" />    
+        </el-drawer>
+    </div>
+</template>
+
+
+
+<style src = "@/css/userprofile.css" scoped></style>
