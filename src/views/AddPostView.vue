b983d6ebceb1da8daa436cb87c0a7d49228a795a
Sarakurachi91 <67615304+Sarakurachi91@users.noreply.github.com>
2023-09-17 19:31:29 +1000

    cancel button
    cancel button

--- src/views/AddPostView.vue
+++ src/views/AddPostView.vue
@@ -1,44 +1,9 @@
 <!-- Add Post Page -->
-<template>
-    <div class="container">
-        <el-container>
-            <el-header class="header">
-                <input type="submit" value="Post" class="postButton" @click="clicked_postButton"> 
-            </el-header>
-            <el-main class="main">
-
-                <div class="user_icon_name">
-                    <!-- NOTE: After Flyger figures out how to display the avatar images by using Vue-dataset, the hard-code below will be updated in the future. -->
-                    <img :src="avatar1" alt="user_icon" class="user_icon" />
-                    <p class="userName">{{ user.name }}</p>
-                </div>
-
-                <input type="text" v-model="postTitle" name="post_title" class="post_title" placeholder="Set a title here..."><br>
-                <textarea v-model="postContent" class="post_content" name="post_content" placeholder="What's on your mind?" rows="17" cols="50" maxlength="500" autofocus required></textarea><br>
-
-
-                <div class="scrollable-container"> 
-                    <input type="file" @change="handleImageUpload" accept="image/*" ref="fileInput" style="display: none" multiple />
-                    
-                    <!-- Display selected images -->
-                    <div v-for="(image, index) in selectedImages" :key="index" class="image-container">
-                        <img :src="image.url" alt="Selected Image" />
-                        <Icon class="cancel_icon" @click="removeImage(index)"><CancelFilled /></Icon>
-                    </div>
-                </div>
-                <!-- Add button to trigger file input -->
-                <Icon class="image_icon" @click="openFileInput" :disabled="selectedImages.length >= 4"><ImageRound /></Icon>
-
-            </el-main> 
-        </el-container>
-    </div>
-</template>
-
 
 <script>
 
 import { ref } from 'vue';
-import { ImageRound, CancelFilled } from '@vicons/material';
+import { ImageRound } from '@vicons/material';
 import { Icon } from '@vicons/utils'
 import axios from 'axios';
 import { mapGetters } from 'vuex';
@@ -86,15 +51,17 @@ import { mapGetters } from 'vuex';
             },
             beforeDrawerClose(done) {
                 done();
-            },
+            }, 
             async clicked_postButton() {
 
                 await this.createPost();
 
-           },
+            },
+            clicked_cancelButton() {
+                this.$router.push('/Community');
+            }
 
 ///
-
             async createPost() {
                 try {
                 const userId = this.loggedInUser.userId;
@@ -181,10 +148,44 @@ import { mapGetters } from 'vuex';
         },
         components: {
             Icon,
-            ImageRound,
-            CancelFilled
+            ImageRound
         }
     };
 </script>
+
+<template>
+    <div class="container">
+        <el-container>
+            <el-header class="header">
+                <input type="submit" value="Post" class="postButton" @click="clicked_postButton"> 
+            </el-header>
+            <el-main class="main">
+
+                <div class="user_icon_name">
+                    <!-- NOTE: After Flyger figures out how to display the avatar images by using Vue-dataset, the hard-code below will be updated in the future. -->
+                    <img :src="avatar1" alt="user_icon" class="user_icon" />
+                    <p class="userName">{{ user.name }}</p>
+                </div>
+
+                <input type="text" v-model="postTitle" name="post_title" class="post_title" placeholder="Set a title here..."><br>
+                <textarea v-model="postContent" class="post_content" name="post_content" placeholder="What's on your mind?" rows="17" cols="50" maxlength="500" autofocus required></textarea><br>
+
+
+                <div class="scrollable-container"> 
+                    <input type="file" @change="handleImageUpload" accept="image/*" ref="fileInput" style="display: none" multiple />
+                    
+                    <!-- Display selected images -->
+                    <div v-for="(image, index) in selectedImages" :key="index" class="image-container">
+                        <img :src="image.url" alt="Selected Image" />
+                        <input type="submit" value="Cancel" class="cancelButton" @click="clicked_cancelButton"> 
+                    </div>
+                </div>
+                <!-- Add button to trigger file input -->
+                <Icon class="image_icon" @click="openFileInput" :disabled="selectedImages.length >= 4"><ImageRound /></Icon>
+
+            </el-main> 
+        </el-container>
+    </div>
+</template>
 
 <style src = "@/css/addPost.css" scoped></style>
