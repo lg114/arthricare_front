a982b1e285e1839b2ccb3190806fd34f47878850
Don1060 <Donovanho95@gmail.com>
2023-09-20 16:08:22 +1000

    update of chatview

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,12 +1,35 @@
 <!-- Chat Page -->
 <!-- NOTE: Run the page on a smartphone, and check if a keyboard comes up on the screen when a user clicks the input fields. -->
+
 <script>
     import { ref } from 'vue';
     import { ArrowBackFilled, CameraAltFilled, ImageRound, MicRound, ArrowCircleUpTwotone } from '@vicons/material';
     import { Icon } from '@vicons/utils'
+    import axios from 'axios';
+   // import VueNativeSock from 'vue-native-websocket';
+   // import SockJS from 'sockjs-client';
+   // import { Stomp } from '@stomp/stompjs';
+   // import { Client } from '@stomp/stompjs';
+
     export default{
         mounted() {
-            document.title = "Chat | ArthriCare";
+            document.title = "Chat | ArthriCare"
+            // Connect to WebSocket when the component is mounted
+                this.connectWebSocket();
+
+            // Retrieve chat channel information from sessionStorage
+            const chatChannelInfor = JSON.parse(sessionStorage.getItem('chatChannelInfor'));
+
+            if (chatChannelInfor) {
+            const userName = chatChannelInfor.userToId.split('-')[1];
+            this.chatPartnerName = userName;
+            } else {
+            this.chatPartnerName = 'Chatting Partner';
+            }
+
+            // Fetch chat history when the component is mounted
+            this.fetchChatHistory();
+
         },
         setup(){
             const avatar1 = ref()
@@ -19,6 +42,8 @@
             avatar3.value = require('@/assets/friend_2.png')
             avatar4.value = require('@/assets/friend_3.png')
             avatar5.value = require('@/assets/friend_4.png')
+
+
             return {
                 avatar1,
                 avatar2,
@@ -52,9 +77,89 @@
                 messageInput: '', // Add this property to hold the message input value
                 chatHistory: [], // Add this property to store chat history
                 drawer: ref(false),
+                chatPartnerName: '', // Initialize with the chat partner's name
+                chatMessages: [], // Array to store chat messages
+                
             };
         },
         methods:{
+
+            // Function to connect to WebSocket
+            connectWebSocket() {
+            // Connect to WebSocket manually
+                this.$connect();
+                },
+            // Function to fetch chat history from the backend
+             fetchChatHistory() {
+             const chatChannelInfor = JSON.parse(sessionStorage.getItem('chatChannelInfor'));
+             if (!chatChannelInfor) {
+                console.error('Chat channel information not found in session storage');
+                return;
+            }
+            const channelId = chatChannelInfor.channelId;
+
+            axios.get(`/ComityChat/getChatHistory?channelId=${channelId}`)
+                .then((response) => {
+                if (response.status === 200) {
+                    const chatHistories = response.data;
+                    chatHistories.forEach((history) => {
+                    const sender = history.from;
+                    const content = history.content;
+                    this.displayMessage(sender, content);
+                    });
+                } else {
+                    console.error('Failed to fetch chat histories');
+                }
+                })
+                .catch((error) => {
+                console.error('Error fetching chat history:', error);
+                });
+            },
+
+                // Function to send a chat message via WebSocket
+                sendMessage() {
+                if (this.messageInput.trim() === '') {
+                    return;
+                }
+
+                const chatMessage = {
+                    from: 'me', // Update with your user identifier
+                    content: this.messageInput,
+                    // Add any other properties needed by your backend
+                };
+
+                this.$socket.sendObj(chatMessage);
+
+                // Push the new message to the chatMessages array
+                this.chatMessages.push({
+                    sender: 'me', // Update with your user identifier
+                    content: this.messageInput,
+                    timestamp: new Date().toISOString(),
+                });
+
+                // Clear the message input
+                this.messageInput = '';
+                },
+
+                // Function to handle incoming WebSocket messages
+                handleIncomingMessage(receivedMessage) {
+                const content = receivedMessage.content;
+                const sender = receivedMessage.from;
+                this.displayMessage(sender, content);
+                },
+                
+                // Function to display a chat message in your chat interface
+                displayMessage(sender, content) {
+                // Create a new message object and push it to the chatMessages array
+                this.chatMessages.push({
+                    sender: sender,
+                    content: content,
+                    timestamp: new Date().toISOString(),
+                });
+                },
+            
+
+
             formatDateAndTime(timestamp) {
         const options = {
             day: '2-digit', // Display day in two digits (01, 02, etc.)
@@ -79,28 +184,22 @@
                 done();
             },
             // New method to send a message and update chat history
-            sendMessage(){
+/*  sendMessage(){
 
                 console.log('sendMessage method called');
                 if (this.messageInput.trim() === '')
                 {return;}
 
-                // demonstration
-                const newMessage = {
-                    sender: 'me', // You can set the sender
-                    content: this.messageInput,
-                    timestamp: new Date().toISOString(),
-                };
                 console.log('New message:', newMessage);
                 // Push the new message to the chat history
                     this.chatHistory.push(newMessage);
                     console.log('Chat history:', this.chatHistory);
 
-
                 // Clear the message input
                 this.messageInput = '';
 
             },
+*/
             //Router
             goToUserProfile(){
                 this.$router.push('/UserProfile');
@@ -126,7 +225,6 @@
         }
     };
 </script>
-
 <template>
     <div class="container">
         <el-container>
