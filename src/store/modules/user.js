4f3c25feabb87741b8ebe86f376236f4b21de543
Lin Gan <92200217+lg114@users.noreply.github.com>
2023-10-21 13:07:42 +1100

    Comment name by gc

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,7 +4,7 @@
 // store/user.js
 import axios from 'axios';
 import router from '@/router';
-import { ElMessage } from 'element-plus';
+import { Snackbar } from '@varlet/ui';
 
 const state = {
   loggedInUser: null,
@@ -35,10 +35,10 @@ const actions = {
     async loginUser({ commit }, { email, password }) {
         try {
             if( email === '' || password === ''){
-                ElMessage.error('Email or Password is not valid');
+                Snackbar.error('Email or Password is not valid');
             }else{
                 if (!actions.isValidEmail(email)){
-                    ElMessage.error('Invalid email format');
+                    Snackbar.error('Invalid email format');
                     return;
                 }
 
@@ -49,26 +49,26 @@ const actions = {
                 const user = response.data; //user information
                 commit('setLoggedInUser', user); //set user information
                 router.push('/Home');
-                ElMessage.success('Login successful!');
+                Snackbar.success('Login successful!');
             }
         } catch (error) {
             console.error('Login failed!', error);
-            ElMessage.error('Login failed');
+            Snackbar.error('Login failed');
         }
     },
     async registerUser({ commit }, userData) {
         try {
             if(userData.email === '' || userData.password === ''){
-                ElMessage.error('Email and Password are required!');
+                Snackbar.error('Email and Password are required!');
             }else{
                 if (!actions.isValidEmail(userData.email)){
-                    ElMessage.error('Invalid email format');
+                    Snackbar.error('Invalid email format');
                     return;
                 }
                 const response = await axios.post('http://localhost:8181/api/register', userData);
                 if (response.status === 200) {
                     console.log('Sign up successful');
-                    ElMessage.success('Registration successful!');
+                    Snackbar.success('Registration successful!');
                     router.push('/Login');    
                     commit('clearLoggedInUser');
                     return true;
@@ -78,7 +78,7 @@ const actions = {
                 }
             }
         } catch (error) {
-            ElMessage.error("This email might be exist!");
+            Snackbar.error("This email might be exist!");
             console.error('Registration failed!', error);
             return false;
         }
@@ -86,10 +86,10 @@ const actions = {
     async resetPassword(commit, { email, newPassword }) {
         try {
             if( email === '' || newPassword === ''){
-                ElMessage.error('Email and New password are required!');
+                Snackbar.error('Email and New password are required!');
             }else{
                 if (!actions.isValidEmail(email)){
-                    ElMessage.error('Invalid email format');
+                    Snackbar.error('Invalid email format');
                     return;
                 }
                 const response = await axios.put('http://localhost:8181/api/updatePasswordbyemail', {
@@ -98,7 +98,7 @@ const actions = {
                 });
                 console.log(response.data);
                 console.log('Password reset successful');
-                ElMessage.success('Reset password successful!');
+                Snackbar.success('Reset password successful!');
                 router.push('/');
                 commit('clearLoggedInUser');
                 return true;
@@ -111,7 +111,7 @@ const actions = {
     async logout({ commit }){
         await commit('clearLoggedInUser');
         await router.push('/');
-        ElMessage.success('Logged out successfully');
+        Snackbar.success('Logged out successfully');
     }
 };
 
